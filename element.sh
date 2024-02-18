24003acb894f6abf963b01546d31c516c687eb68
Allie Code <olympia.s314@gmail.com>
2024-02-18 10:19:35 +0000

    fix: remove format error

--- element.sh
+++ element.sh
@@ -30,7 +30,7 @@ OUTPUT(){
   GET_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$GET_ATOMIC_NUMBER")
   GET_MELTING=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$GET_ATOMIC_NUMBER")
   GET_BOILING=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$GET_ATOMIC_NUMBER")
-  echo -e "\nThe element with atomic number $GET_ATOMIC_NUMBER is $GET_NAME ($GET_SYMBOL). It's a $GET_TYPE, with a mass of $GET_MASS amu. $GET_NAME has a melting point of $GET_MELTING celsius and a boiling point of $GET_BOILING celsius."
+  echo  "The element with atomic number $GET_ATOMIC_NUMBER is $GET_NAME ($GET_SYMBOL). It's a $GET_TYPE, with a mass of $GET_MASS amu. $GET_NAME has a melting point of $GET_MELTING celsius and a boiling point of $GET_BOILING celsius."
   fi
 }
 
