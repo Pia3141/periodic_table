950f64a5583569c6696f42c4fd7e1e3d5e910c92
Allie Code <olympia.s314@gmail.com>
2024-02-18 09:38:47 +0000

    feat: check if input

---
+++ element.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo Please provide an element as an argument.
+else
+echo $1
+  OUTPUT
+fi
+
